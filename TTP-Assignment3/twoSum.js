12ab91ac8453a675f10ecd3896732844db0c3282
John Santiago <santiago.john96@gmail.com>
2023-06-21 15:09:31 -0400

    Complete functions and tests

---
+++ TTP-Assignment3/twoSum.js
@@ -0,0 +1,18 @@
+// Function to find indices of two numbers that add up to the target
+function twoSum(nums, target) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (complement in map) {
+      return [map[complement], i];
+    }
+
+    map[nums[i]] = i;
+  }
+
+  return [];
+}
+
+module.exports = twoSum;
